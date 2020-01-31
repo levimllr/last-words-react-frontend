42fe8b71456dbdb71e70c5f36e5ac0f22b533825
Levi Miller <paullevimiller@gmail.com>
2020-01-30 16:46:30 -0800

    Updates

---
+++ precache-manifest.53ef03811e7b35a3be6bc46227958dae.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df404dc17f1926d53159449f4b6a8a05",
+    "url": "/last-words-react-frontend/index.html"
+  },
+  {
+    "revision": "8dd59ed217ecd0794a6c",
+    "url": "/last-words-react-frontend/static/css/main.90d29ccc.chunk.css"
+  },
+  {
+    "revision": "6cfef2fc49b59f83fbef",
+    "url": "/last-words-react-frontend/static/js/2.e7a0a151.chunk.js"
+  },
+  {
+    "revision": "8dd59ed217ecd0794a6c",
+    "url": "/last-words-react-frontend/static/js/main.1be17eb7.chunk.js"
+  },
+  {
+    "revision": "c9c57384f09dd0976486",
+    "url": "/last-words-react-frontend/static/js/runtime-main.b7475ca2.js"
+  }
+]);
